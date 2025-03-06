cc1de5a2b0aa521472b530bde122d45a43199c24
viniciusjn <viniciusaqpsjn@gmail.com>
2025-03-06 16:31:07 -0300

    Header config

--- ves-client/src/app/components/layout/Header.tsx
+++ ves-client/src/app/components/layout/Header.tsx
@@ -3,15 +3,15 @@ import Image from "next/image";
 
 export default function Header() {
     return (
-        <header className="fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-t from-transparent to-black p-2 px-4 transition-all lg:px-16 lg:py-4">
+        <header className="fixed top-0 z-50 flex w-full items-center justify-between bg-gradient-to-b from-black/70 p-2 px-4 transition-all lg:px-16 lg:py-4">
             <Link href="/">
                 <Image src="/VES_logo.png" alt="logo Netflix" width={70} height={70} className="cursor-pointer"/>
             </Link>
             <div className='flex items-center space-x-2 md:space-x-8'>
                 <nav>
                     <ul className="hidden md:flex md:space-x-6 hover:underline-offset-1">
-                        <Link href="/"><li className="cursor-pointer p-2 hover:bg-purple-700 hover:rounded">Sobre</li></Link>
-                        <Link href="/"><li className="cursor-pointer p-2 hover:bg-purple-700 hover:rounded">Contato</li></Link>
+                        <Link href="/"><li className="cursor-pointer p-2 hover:bg-[#035EFC] hover:rounded">Sobre</li></Link>
+                        <Link href="/"><li className="cursor-pointer p-2 hover:bg-[#035EFC] hover:rounded">Contato</li></Link>
                     </ul>
                 </nav>
             </div>
