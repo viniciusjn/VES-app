9e5932ab36cf480b2cacdc7e820ec67b8e7c7273
viniciusjn <viniciusaqpsjn@gmail.com>
2025-03-06 15:44:43 -0300

    Footer config

--- ves-client/src/app/components/layout/Footer.tsx
+++ ves-client/src/app/components/layout/Footer.tsx
@@ -1,35 +1,37 @@
 import Link from "next/link";
-import { FaFacebookF, FaTwitter, FaInstagram, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaTwitter, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export default function Footer() {
+
+  const whatsappNumber = "5532999701993";
+  const whatsappMessage = "Olá! Gostaria de mais informações sobre um orçamento.";
+  const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
+
   return (
     <footer className="bg-zinc-900 text-white py-8 mt-auto">
-      <div className="container mx-auto flex flex-col items-center text-center space-y-6 px-6">
-        <h2 className="text-2xl font-bold tracking-wider">VPg</h2>
+        <div className="container mx-auto flex flex-col items-center text-center space-y-6 px-6">
+            <h2 className="text-2xl font-bold tracking-wider">Vênus Energia Solar</h2>
 
-        <nav className="flex space-x-6 text-sm font-medium">
-          <Link href="/" className="hover:text-purple-700 transition">Home</Link>
-          <Link href="#" className="hover:text-purple-700 transition">About</Link>
-          <Link href="/ranking" className="hover:text-purple-700 transition">Ranking</Link>
-        </nav>
+            <nav className="flex space-x-6 text-sm font-medium">
+                <Link href="/" className="hover:text-[#035EFC] transition">Início</Link>
+                <Link href="/" className="hover:text-[#035EFC] transition">Sobre</Link>
+                <Link href="/" className="hover:text-[#035EFC] transition">Contato</Link>
+            </nav>
 
-        <div className="flex space-x-4">
-          <Link href="#" className="p-2 bg-gray-800 rounded-full hover:bg-purple-700 transition">
-            <FaFacebookF className="w-5 h-5" />
-          </Link>
-          <Link href="#" className="p-2 bg-gray-800 rounded-full hover:bg-purple-700 transition">
-            <FaTwitter className="w-5 h-5" />
-          </Link>
-          <Link href="#" className="p-2 bg-gray-800 rounded-full hover:bg-purple-700 transition">
-            <FaInstagram className="w-5 h-5" />
-            </Link>
-          <Link href="#" className="p-2 bg-gray-800 rounded-full hover:bg-purple-700 transition">
-            <FaLinkedinIn className="w-5 h-5" />
-          </Link>
+            <div className="flex space-x-4">
+                <Link href={whatsappUrl} target="_blank" className="p-2 bg-gray-800 rounded-full hover:bg-[#035EFC] transition">
+                    <FaWhatsapp className="w-5 h-5" />
+                </Link>
+                <Link href="https://www.instagram.com/venusenergiasolar?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" target="_blank" className="p-2 bg-gray-800 rounded-full hover:bg-[#035EFC] transition">
+                    <FaInstagram className="w-5 h-5" />
+                </Link>
+                <Link href="https://www.facebook.com/share/1E4tFpQWr8/" target="_blank" className="p-2 bg-gray-800 rounded-full hover:bg-[#035EFC] transition">
+                    <FaFacebookF className="w-5 h-5" />
+                </Link>   
+            </div>
+
+            <p className="text-xs text-gray-400">©{new Date().getFullYear()} Vênus Energia Solar. Todos os direitos reservados.</p>
         </div>
-
-        <p className="text-xs text-gray-400">© {new Date().getFullYear()} VPg. All rights reserved.</p>
-      </div>
     </footer>
   );
 }
