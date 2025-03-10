d0254bf449964cbda229c9b7922e9fe6e8ddd927
viniciusjn <viniciusaqpsjn@gmail.com>
2025-03-10 14:53:09 -0300

    Benefits component

---
+++ ves-client/src/app/components/Benefits.tsx
@@ -0,0 +1,38 @@
+import { CurrencyDollarIcon, HomeIcon, GlobeAmericasIcon, ChartBarIcon } from '@heroicons/react/24/solid';
+
+const features = [
+  { icon: CurrencyDollarIcon, 
+    title: "Economize Dinheiro", 
+    description: "Com a instalação dos painéis solares, você pode economizar em até 95% sua conta de luz." },
+  { icon: HomeIcon, 
+    title: "Valorize seu Imóvel", 
+    description: "Os painéis e todo o sistema instalado em sua casa, podem contriubuir para um diferencial de mercado e valorizar seu imóvel." },
+  { icon: GlobeAmericasIcon, 
+    title: "Ajude o Meio Ambiente", 
+    description: "A energia gerada pelos painéis solares é considerada limpa e sustentável, contribuindo para o bem do nosso meio ambiente." },
+  { icon: ChartBarIcon, 
+    title: "Proteja-se Contra a Inflação", 
+    description: "Por vários motivos diferentes, sejam políticos, econômicos ou naturais, a conta de energia sofre diversas alterações aumentando cada vez mais seus valores." }
+];
+
+export default function Benefits() {
+  return (
+    <section className="text-white py-16 h-[105vh]">
+      <div className="container mx-auto px-6 text-center">
+        <h2 className="text-6xl font-bold">Quais são os principais benefícios ao utilizar energia solar?</h2>
+
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-8 mt-12">
+          {features.map((feature, index) => (
+            <div key={index} className="flex flex-col items-center text-center">
+              <div className="bg-gray-800 p-10 rounded-full">
+                <feature.icon className="h-30 w-30 text-[#035EFC]" />
+              </div>
+              <h3 className="text-4xl font-semibold mt-4">{feature.title}</h3>
+              <p className="text-xl text-gray-400 mt-2">{feature.description}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
--- ves-client/src/app/page.tsx
+++ ves-client/src/app/page.tsx
@@ -2,15 +2,20 @@ import Image from "next/image";
 import PageLayout from "./components/layout/PageLayout";
 import { Banner } from "./components/Banner";
 import AboutUs from "./components/AboutUs";
+import Benefits from "./components/Benefits";
 
 export default function Home() {
   return (
     <PageLayout>
+    
       <div className="relative p-8 pb-0.5 w-full lg:px-16">
         <Banner />
       </div>
 
-    <AboutUs />
+      <Benefits />
+
+      <AboutUs />
+      
     </PageLayout>
   );
 }
