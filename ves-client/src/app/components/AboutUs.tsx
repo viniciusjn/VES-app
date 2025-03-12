4b063134497304303237eba12d3a5f0f6ca99372
viniciusjn <viniciusaqpsjn@gmail.com>
2025-03-12 10:35:53 -0300

    AboutUs config

--- ves-client/src/app/components/AboutUs.tsx
+++ ves-client/src/app/components/AboutUs.tsx
@@ -9,16 +9,14 @@ export default function AboutUs() {
   const whatsappUrl = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(whatsappMessage)}`;
 
   return (
-    <section className="relative w-full z-0 lg:h-[70vh] h-[90vh] bg-fixed bg-center bg-cover flex items-center justify-center text-white"
+    <section className="relative w-full z-0 lg:h-[70vh] md:h-[65vh] h-[90vh] bg-fixed bg-center bg-cover flex items-center justify-center text-white"
     style={{
       backgroundImage: "url('https://assets.science.nasa.gov/content/dam/science/esd/eo/content-feature/nightlights/images/media/BlackMarble_2016_Americas_composite.png')", 
     }}>
       <div className="absolute inset-0 bg-black/75"></div>
       
 
-      {/* Content */}
       <div className="relative z-10 flex flex-col md:flex-row items-center max-w-5xl mx-auto p-6">
-        {/* Left Side - Main Text */}
         <div className="md:w-2/3">
           <h1 className="text-3xl font-bold">
             <span className="border-l-4 border-[#035EFC] pl-3 text-5xl">Sobre a <a className="text-yellow-500">Vênus</a></span>
@@ -34,7 +32,6 @@ export default function AboutUs() {
           </Link>
         </div>
 
-        {/* Right Side - Services List */}
         <div className="md:w-2/5 mt-8 md:mt-0 md:ml-10 bg-[#171717] bg-opacity-75 p-6 rounded-lg">
           <h2 className="text-xl font-bold text-[#035EFC]">Nossos serviços</h2>
           <div className="mt-4 space-y-4">
@@ -69,6 +66,7 @@ export default function AboutUs() {
           </div>
         </div>
       </div>
+      <div className="absolute bottom-0 w-full h-50 bg-gradient-to-t from-[#171717] to-transparent"></div>
     </section>
   );
 }
